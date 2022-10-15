cf8e0443c3a4bb39966a53880f5b9f3efc760cfb
vranicmitar <mitarvranic0@gmail.com>
2022-10-15 18:45:21 +0200

    #update

--- js/1. cas/app.js
+++ js/1. cas/app.js
@@ -58,6 +58,66 @@ console.log("Ovo je nas prvi ispis u conzoli");
 
     // 6. null - je tip podatka za nepostojecu promenljivu ili za promenljivu kojoj smo dodelili nekorektnu vrdnost.
 
+    let skola = null;
+    console.log(skola);
+    console.log(typeof skola);  // rezultat je Object
+    
+    // 7. Symbol - novi tip podatka u JS koji se koristi za dodeljivanje jedinstvene ili anonimne vrednosti.
+
+    x = Symbol("Danas je lepo vreme.");
+    console.log(x);
+    y = Symbol("Danas je lepo vreme.");
+    console.log(y);
+
+    console.log(x === y);  // razlikuju se 
+
+    // PRIMITIVNI TIPOVI PODATAKA SE KORISTE KADA IMAMO PROMENLJIVU ODNOSNO VARIJABLU SA SAMO JEDNOM VREDNOSCU
 
 
-    console.log (typeof);
+// Neprimitivni tipovi podataka:
+
+// ZA PROMENLJIVE (VARIJABLE) KOJE MOGU SKLADISTITI VISE VREDNOSTI, ILI NEKE KOMPLEKSNE VREDNOSTI 
+//  SE KORISTE NEPRIMITIVNI TIPOVI PODATAKA.
+
+// Kada ispitujemo tip podatka za neku neprimitivnu promenljivu u JS dobijamo Object za tip.
+
+    // 1. Nizovi (Arrays) - neprimitivni tip podatka koji nam sluzi za skladistenje vise vrednosti.
+    // U JS niz moze sadrzati elemente razlicitog tipa.
+
+    niz1 = ["Mitar", "Dzenan", "Haris", "Hamed", "Nikola"];
+    console.log(niz1);
+
+    niz2 = ["Hamed", 15, false];
+    console.log(niz2);
+
+    // Jos jedan nacin definisanja niza:
+    
+    niz3 = new Array("Mitar", "Dzenan", "Haris", "Hamed", "Nikola");
+    console.log(niz3);
+
+    // 2. Objekti (Object) - su GLAVNI neprimitivni tip podatka u JS.
+    // Objekti se zapisuju po principu name:value pair (ime:vrednost par).
+
+    obj1 = {
+        ime: "Mehmed",
+        prezime: "Kucevic",
+        godine: 15,
+        skola: "Medicinska skola",
+        punoletnost:false
+    }
+
+    console.log(obj1.godine);
+
+    // 3. Setovi (Sets) - slicni nizovima sa razlikom da set ne moze da sadrzi duplikate.
+    set1 = new Set(["Alen", "Hasan", "Miona","Amir", "ALen"]); // nisu dozvoljeni duplikati
+    console.log(set1);
+    
+    // 4. Mape (Maps) - su slicni objektima sa nesto drugacijim nacinom definisanja.
+
+    map1 = new Map([
+        ["jabuke", 50],
+        ["kruske", 60],
+        ["jagode", 80]
+    ])
+    
+    console.log(map1);
