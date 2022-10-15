add5dcefc73ba29dfec249f830f27dd407cfe7d8
vranicmitar <mitarvranic0@gmail.com>
2022-10-15 19:17:43 +0200

    #update

---
+++ js/2.cas/main.js
@@ -0,0 +1,51 @@
+// JS moze prikazati podatke na nekoliko nacina:
+
+    // 1. innerHTML
+
+        document.getElementById("p").innerHTML = "MPQ";
+
+    // 2. write
+
+        document.write() // predstavlja najcesci nacin za testiranje nekog koda
+
+    // 3. alert - upozorenje
+
+        alert("SIIIIIIIIIIIIIIIIIIUUUUUUUUUUUUUUUUUU");
+
+    // 4. console.log() - ispisivanje u konzoli
+
+        console.log("Poruka za ispis u konzoli");
+
+// Pravila nazivanja promenljivih:
+
+// Promenljiva u JS mora poceti sa nekim od sled. znakova:
+
+    // 1. Slovo (veliko ili malo) A-Z ili a-z
+    // 2. $ znak moze biti pocetni karakter neke promenljive
+    // 3. _ 
+
+// Broj moze biti sadrzan u nazivu neke promenljive ali promenljiva NE SME da pocne brojem.
+
+// prom1 = "neki string"; dobar naicn
+// 2prom = "drugi string"; nije korektan nacin
+
+// prosekOcena = 4,14; preporucljiv nacin
+// prosek ocena = 4,14; nije korektno
+// prosek_ocena = 5,00; dobar nacin
+
+
+// Postoje 4 nacina za deklarisanje neke promenljive:
+
+    // 1. Dodeljivanje vrednosti bez da smo prethodno izvrsili deklaraciju
+    // prom = 12; 
+
+    // 2. Koriscenjem var keyword (rezervisane reci) - var je koriscena na starijim browserima (sve se manje koristi)
+    // var a = 5;
+
+    // 3. Koriscenjem let keyword (rezervisane reci) - let se najcesce koristi kada postoji sansa da se vrdnost date promenljive menja.
+    
+    // 4. Koriscenje const keyword (rezervisane reci) - const se koristi za definisanje konstante koje nece menjati vrednost.
+
+    // 5. Promenljive (varijable) predstavljaju kontejnere za skladistenje vrdnosti.
+    
+
