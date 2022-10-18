885fa992ec4744cf7355b0c962f14a0f0934ecdf
vranicmitar <mitarvranic0@gmail.com>
2022-10-18 21:13:07 +0200

    #update JS

---
+++ js/3.cas/main2.js
@@ -0,0 +1,63 @@
+// OPERATORI U JS
+// Podelicemo operatore u 6 grupa:
+
+// 1. Aritmeticki operatori (Aritmetic operators);
+// 2.Operatori dodele vrednosti (Assigment operators);
+// 3.String operatori (String operators);
+// 4.Operatori poredjenja (Comparison operators);
+// 5.Logicke operatore (Logical operators);
+// 6.Tip operatora (Type operators).
+
+// PRIMERI:
+
+// 1. Aritmeticki operatori (Aritmetic operators);
+// 1.1 + OPERATOR sabiranje
+console.log(4 + 5);
+// 1.2 - OPERATOR oduzimanje
+console.log(-15 - 10);
+// 1.3 * OPERATOR mnozenje
+console.log(5 * 5);
+// 1.4 / OPERATOR deljenja
+console.log(20 / 5);
+// 1.5 ** OPERATOR stepenovanja
+console.log(5 ** 3);
+// 1.6 % OPERATOR modula (ostatak pri deljenju)
+console.log(12 % 5); // rezultat je 2
+console.log(12 % 6); // rezultat je 0
+
+// Uslov za parnost :
+// i % 2 = 0;
+// Uslov za neparnost :
+// i % 2 = 1;
+
+// 1.7 % OPERATOR Increment (povecava vrednost za 1)
+let g = 5;
+// postfiksni operator !!!
+g++; // g = g + 1
+console.log(g);
+
+let y = 11;
+// prefiksni operator !!!
+console.log(++y);
+
+// 1.8 % OPERATOR Decrement (smanjuje vrednost za 1)
+let u = 99;
+// postfiksni operator !!!
+u--;
+console.log(u);
+
+let w = 65;
+// prefiksni operator !!!
+console.log(--w);
+
+
+
+
+
+
+
+
+
+
+
+
