3698ff3427623965aea8226655ae3c850109725c
vranicmitar <mitarvranic0@gmail.com>
2022-10-28 01:34:30 +0200

    #update

---
+++ js/6.cas/domaci.js
@@ -0,0 +1,28 @@
+// 1.zadatak - Ispisati neparne brojeve od 1 do 20 -->
+
+for (let i = 1; i<=20; i += 2) {
+    console.log(i);
+}
+
+// 2.zadatak - Ispisati sve brojeve od 50 do 100 koji su deljivi sa 50 -->
+
+for (let i = 50; i<=100; i++) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
+
+// 3.zadatak - Ispisati dvostruku vrednost brojeva od 6 do 14 -->
+
+
+// for (let i = 6; i<=14; i++) {
+//     console.log (i *= 2);
+// }
+
+// 4.zadatak - Suma neparnih prirodnih brojeva od 10 do 20 -->
+
+for (let i = 10; i<20; i++) {
+    if (i % 2 === 0) {
+        console.log (i+i+i+i+i);
+    }
+}
