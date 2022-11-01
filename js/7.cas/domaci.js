75caf8d8b97971045c8feb50358b9e5a083c9186
vranicmitar <mitarvranic0@gmail.com>
2022-11-01 19:20:44 +0100

    #homework

--- js/7.cas/domaci.js
+++ js/7.cas/domaci.js
@@ -1,36 +1,41 @@
-const dan = prompt("Unesite dan: ");
-switch (dan) {
-  case ponedeljak:
-    console.log("Danas je 'radni dan'");
-    break;
-  case utorak:
-    console.log("Danas je 'radni dan'");
-    break;
-  case sreda:
-    console.log("Danas je 'radni dan'");
-    break;
-  case cetvrtak:
-    console.log("Danas je 'radni dan'");
-    break;
-  case petak:
-        console.log("Danas je 'radni dan'");
-        break;
-   case subota:
-    console.log("Ugodno provedite vikend" );
-    break; 
-    case nedelja:
-        console.log("Ugodno provedite vikend" );
-        break;  
-  default:
-    console.log("Nije unet trazeni dan.");
-}
-
-
 // Domaci zadatak:
 // Preko switch naredbe:
-console.log(new Date().getDay());
+// console.log(new Date().getDay());
 // getDay() - daje vrednost 0-6
 // switch (new Date().getDay())...
 // Na osnovu vrednosti koju nam daje izraz iz switch naredbe, kroz case_ove i eventualno default ispisati poruku:
 // "Danas je 'radni dan'"
 // "Ugodno provedite vikend" 
+
+const dani = +prompt("Unesite broj vaseg omiljenog dana od 0-6: ");
+switch (dani) {
+  case 0:
+    console.log("Ugodno provedite vikend");
+    break;
+
+  case 1:
+    console.log("Danas je radan dan");
+    break;
+
+  case 2:
+    console.log("Danas je radan dan");
+    break;
+
+  case 3:
+    console.log("Danas je radan dan");
+    break;
+  case 4:
+    console.log("Danas je radan dan");
+    break;
+
+  case 5:
+    console.log("Danas je radan dan");
+    break;
+
+  case 6:
+    console.log("Ugodno provedite vikend");
+    break;
+      
+    default:
+    console.log("Niste uneli odgovarajuci broj od 0 - 6 !!!");
+}
