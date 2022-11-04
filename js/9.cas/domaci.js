0254e27728ab14178846f2e942c33cbc3989f7d4
vranicmitar <mitarvranic0@gmail.com>
2022-11-04 19:35:58 +0100

    #update not finish

---
+++ js/9.cas/domaci.js
@@ -0,0 +1,11 @@
+let recenica = prompt("Unesi neku recenicu: ");
+const duzina = recenica.length;
+
+for (i = 0; i < duzina; i++) {
+    if (recenica[i] === "a") {
+        
+    } else if (recenica[i] === "s") {
+        continue;
+    }
+    console.log(recenica[i]);
+}
