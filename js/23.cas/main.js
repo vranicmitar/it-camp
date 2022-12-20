d44644bad869500ebdcc04f4b268ddf005f5499b
vranicmitar <mitarvranic0@gmail.com>
2022-12-20 21:14:16 +0100

    #update

---
+++ js/23.cas/main.js
@@ -0,0 +1,31 @@
+// Number Properties - Konstante //
+
+// Sledecim konstantama mozemo pristupiti jedino pomocu Number -->
+
+// Number.PropertyName
+// Napomena --> KOnstante ne mogu biti primenjene na varijable !!!
+
+// 1.MAX_VALUE - Vraca najveci broj u JS
+
+const najveci = Number.MAX_VALUE;
+console.log(najveci);
+
+// 2.MIN_VALUE - Vraca najmanji broj u JS
+
+const najmanji = Number.MIN_VALUE;
+console.log(najmanji);
+
+// 3.POSITIVE_INFINITY - Vraca Infinity u JS
+
+const infinity = Number.POSITIVE_INFINITY;
+console.log(infinity)
+
+// 4.NEGATIVE_INFINITY - Vraca Infinity u JS
+
+const minusinfinity = Number.NEGATIVE_INFINITY;
+console.log(minusinfinity);
+
+// 5.NaN - Vraca NaN
+
+const notanum = Number.NaN;
+console.log(notanum);
