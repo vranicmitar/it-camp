a8815bc7bca8359467fa00a898ea7bfd6fee32e9
vranicmitar <mitarvranic0@gmail.com>
2022-12-24 14:01:25 +0100

    #homewrok

---
+++ js/24.cas/domaci.js
@@ -0,0 +1,28 @@
+//  Domaci:
+// 1.
+// Napraviti funkciju koja izracunava obim kruga, gde se
+//  poluprecnik unosi od strane korisnika.
+
+function poluprecnik(r1) {
+  let O = 2 * r1 * Math.PI;
+  return Math.trunc(O);
+}
+console.log(poluprecnik(15));
+
+// 2.
+// Napraviti funkciju koja izracunava povrsinu kruga, gde
+//  precnik predstavlja argument funkcije.
+
+function precnik(R) {
+  let r1 = R / 2;
+  let P = r1 ** 2 * Math.PI;
+  return Math.trunc(P);
+}
+console.log(precnik(20));
+
+// 3.
+// Napraviti funkciju za pretvaranje radijana u stepene.
+
+const radijaniUStepene = (radijan) => radijan * (180 / Math.PI);
+
+console.log(radijaniUStepene(0.7853981633974483));
