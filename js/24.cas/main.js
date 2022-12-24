585001457f9e0f69a35a8752ac43c4cfb751df0e
vranicmitar <mitarvranic0@gmail.com>
2022-12-24 14:01:34 +0100

    #update

--- js/24.cas/main.js
+++ js/24.cas/main.js
@@ -6,7 +6,7 @@
 
 // Sintaksa je Math.Property.
 
-// Math konstanti ima 8 : 
+// Math konstanti ima 8 :
 
 // 1.
 // Math.E - vraca Ojlerov broj
@@ -40,15 +40,14 @@ console.log(Math.LOG2E);
 // Math.LOG10E - varca vrednost logartima osnove 10 broja e
 console.log(Math.LOG10E);
 
-
 // Math Methods //
 
-// Sintaksa je : 
+// Sintaksa je :
 // Math.method(number).
 
-// Number to Integer : 
+// Number to Integer :
 
-// 1. Math.round() - vraca blizi ceo broj 
+// 1. Math.round() - vraca blizi ceo broj
 
 console.log(Math.round(22.23));
 console.log(Math.round(22.74));
@@ -68,7 +67,6 @@ console.log(Math.floor(-46.23));
 console.log(Math.trunc(58.23));
 console.log(Math.trunc(-58.23));
 
-
 // Math.sign(numbver) - vraca -1, 0 ili 1 u zavisnosti od toga kog je znaka broj.
 
 console.log(Math.sign(56));
@@ -77,7 +75,7 @@ console.log(Math.sign(0));
 
 // Math.pow(number, power) - vraca number stepenovan na power
 
-console.log(Math.pow(2,15)); // 2**15
+console.log(Math.pow(2, 15)); // 2**15
 
 // Math.sqrt(number) - vraca kvadratni koren broja number
 
@@ -91,28 +89,28 @@ console.log(Math.abs(-2));
 // Math.sin(number) - vraca vrednost sinusa (izmedju -1 i 1). Argument se posmatra kao vrednost u radijanima.
 
 console.log(Math.sin(0));
-console.log(Math.sin(Math.PI/2));
+console.log(Math.sin(Math.PI / 2));
 
 // Math.cos(number) - vraca vrednost kosinusa (izmedju -1 i 1). Argument se posmatra kao vrednost u radijanima.
 
 console.log(Math.cos(0));
-console.log(Math.cos(Math.PI/2));
-
+console.log(Math.cos(Math.PI / 2));
 
 // 32. Write a JavaScript program to find a value which is nearest to 100 from two different given integer values.
 
-// Ponovni zadatak 
+// Ponovni zadatak
 
-const blizi100 = (num1,num2) => {
-    const razlika1 = Math.abs(100 - num1)
-    const razlika2 = Math.abs(100 - num2)
-    const bliziBroj = razlika1 > razlika2 ? num2 : razlika1 < razlika2 ? num1 : null
+const blizi100 = (num1, num2) => {
+  const razlika1 = Math.abs(100 - num1);
+  const razlika2 = Math.abs(100 - num2);
+  const bliziBroj =
+    razlika1 > razlika2 ? num2 : razlika1 < razlika2 ? num1 : null;
 
-    if(bliziBroj === null) {
-        return `Brojevi su jednako udaljeni od broja 100`;
-    } else {
-        return `Blizi je broj ${bliziBroj}`;
-    }
-}
-console.log(blizi100(-150,40));
-console.log(blizi100(-150,-150));
+  if (bliziBroj === null) {
+    return `Brojevi su jednako udaljeni od broja 100`;
+  } else {
+    return `Blizi je broj ${bliziBroj}`;
+  }
+};
+console.log(blizi100(-150, 40));
+console.log(blizi100(-150, -150));
