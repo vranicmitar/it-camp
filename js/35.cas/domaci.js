c10707d5b7439da0271fdbe512df78c5087d5301
vranicmitar <mitarvranic0@gmail.com>
2023-01-19 20:05:58 +0100

    #update

--- js/35.cas/domaci.js
+++ js/35.cas/domaci.js
@@ -6,6 +6,7 @@ const novinizzz = nizzzz
   .filter((element) => element % 2 === 0)
   .reduce((prev, curr) => prev + curr);
 console.log(novinizzz);
+
 // 2. From the array of numbers, choose even double even numbers and compute the sum using Array's filter, map and reduce methods.
 
 const novinizzz2 = nizzzz
@@ -13,8 +14,10 @@ const novinizzz2 = nizzzz
   .map((el) => el ** 2)
   .reduce((prev, curr) => prev + curr);
 console.log(novinizzz2);
+
 // 3. Write a JavaScript program to find the most frequent item of an array.
 // Sample array :
+
 const arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
 function najvisePronadjen(arr1) {
   const newArr1 = [];
@@ -29,9 +32,28 @@ function najvisePronadjen(arr1) {
   const indexNajvisePronadjenog = newArr1.indexOf(najvisePronadjen);
   return `Najvise ima karaktera ${arr1[indexNajvisePronadjenog]}`;
 }
-
 console.log(najvisePronadjen(arr1));
+
+// 2.nacin
+
+let pojavljivanje = 1;
+let element;
+let brojac = 0;
+for (i = 0; i < arr1.length; i++) {
+  for (k = i; k < arr1.length; k++) {
+    if (arr1[i] === arr1[k]) {
+      brojac++;
+      if (brojac > pojavljivanje) {
+        element = arr1[i];
+        pojavljivanje = brojac;
+      }
+    }
+  }
+  brojac = 0;
+}
+console.log(`To je element ${element} i pojavljuje se ${pojavljivanje} puta`);
 // 4. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
 function zameni(recenica) {
   let novarecenica = "";
   for (let i = 0; i < recenica.length; i++) {
