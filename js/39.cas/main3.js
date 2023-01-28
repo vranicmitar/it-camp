6215cee4794935760a543fc39d575c424e757d70
vranicmitar <mitarvranic0@gmail.com>
2023-01-28 19:05:38 +0100

    #JSON update

--- js/39.cas/main3.js
+++ js/39.cas/main3.js
@@ -5,21 +5,51 @@
 // 3. povecanjeBrzine: (metoda koja uzima jedan argument),
 // 4. smanjenjeBrzine: (metoda koja uzima jedan argument),
 // 5. koci: (metoda koja dodeljuje 0 za vrednost trenutne brzine).
- 
-const myCar = {
-    id: 1,
-    marka: "Audi",
-    model: "a4",
-    boja: "Crvena",
-    pogon: "prednji",
-    menjac: "automatski",
-    kontakt: [0622222, 02033322],
-    servis: {
-      datum: "04,maj",
-      km: 23000,
-      serviser: "Pasovic",
-    },
-    udaran: true,
-  };
 
-  
+const myCar = {
+  id: 1,
+  marka: "Audi",
+  model: "a4",
+  boja: "Crvena",
+  pogon: "prednji",
+  menjac: "automatski",
+  kontakt: [0622222, 02033322],
+  servis: {
+    datum: "04,maj",
+    km: 23000,
+    serviser: "Pasovic",
+  },
+  udaran: true,
+};
+
+myCar.trenutnaBrzina = 0;
+myCar.maximalnaBrzina = 260;
+myCar.povecanjeBrzine = function (ubrzanje) {
+  if (this.trenutnaBrzina + ubrzanje > this.maximalnaBrzina) {
+    return `Nije moguce voziti preko makimalne brzine`;
+  } else {
+    this.trenutnaBrzina += ubrzanje;
+  }
+};
+myCar.povecanjeBrzine(30); // menjanje svojstva
+console.log(myCar.povecanjeBrzine(240));
+
+myCar.smanjenjeBrzine = function (smanjenje) {
+  if (this.trenutnaBrzina - smanjenje < 0) {
+    return `Ne mozete smanjiti brzinu za tu vrednost`;
+  } else {
+    this.trenutnaBrzina -= smanjenje;
+  }
+};
+
+console.log(myCar.smanjenjeBrzine(40));
+
+myCar.smanjenjeBrzine(25);
+console.log(myCar); // trenutna brzina je sad 5
+
+myCar.koci = function () {
+  this.trenutnaBrzina = 0;
+};
+
+myCar.koci();
+console.log(myCar);
