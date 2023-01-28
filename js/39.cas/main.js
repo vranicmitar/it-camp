445d1ca9ce6a96c5aae7364147ac08b59e3623fc
vranicmitar <mitarvranic0@gmail.com>
2023-01-28 18:33:00 +0100

    #update

---
+++ js/39.cas/main.js
@@ -0,0 +1,34 @@
+// JSON - JavaScript Object Notation //
+
+// JSON je format za skladistenje i orenis podataka.
+// Najcesce se koristi kada se podaci salju sa servera ka WEB stranici.
+
+// JSON podaci su vrlo slicni JS objektu i oni se zapisuju:
+
+// const person = {
+//     "firstName" : "Amer",
+//     "lastName" : "Honic",
+//     "age" : 19,
+// }
+
+// keys/names zahtevaju duple navodnike za razliku od JavaScript objekta.
+
+const person = {
+  firstName: "Amer",
+  lastName: "Honic",
+  age: 19,
+};
+
+// Dve metode za pretvaranje :
+
+// 1.JSON.stringify() - pretvara nas objekat u JSON format pa JSON u string
+// Koristi se za slanje podataka...
+
+const strObject = JSON.stringify(person);
+console.log(strObject);
+console.log(typeof strObject);
+
+// 2.JSON.parse() - analizira neki string i u slucaju mogucnosti pretvara u objekat (nama citljiv podatak)
+
+const obj = JSON.parse(strObject);
+console.log(obj);
---
+++ js/39.cas/main3.js
@@ -0,0 +1,25 @@
+// Domaci zadatak:
+// Unutar postojeceg objekta dodati nova svojstva:
+// 1. trenutnaBrzina: 0,
+// 2. maksimalnaBrzina: 260,
+// 3. povecanjeBrzine: (metoda koja uzima jedan argument),
+// 4. smanjenjeBrzine: (metoda koja uzima jedan argument),
+// 5. koci: (metoda koja dodeljuje 0 za vrednost trenutne brzine).
+ 
+const myCar = {
+    id: 1,
+    marka: "Audi",
+    model: "a4",
+    boja: "Crvena",
+    pogon: "prednji",
+    menjac: "automatski",
+    kontakt: [0622222, 02033322],
+    servis: {
+      datum: "04,maj",
+      km: 23000,
+      serviser: "Pasovic",
+    },
+    udaran: true,
+  };
+
+  
