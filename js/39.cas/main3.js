0cd169abeb79a6ce0a8ad851060c1f67f44733e0
vranicmitar <mitarvranic0@gmail.com>
2023-01-30 16:50:08 +0100

    #update homework

--- js/39.cas/main3.js
+++ js/39.cas/main3.js
@@ -53,3 +53,71 @@ myCar.koci = function () {
 
 myCar.koci();
 console.log(myCar);
+
+// Domaci zadatak:
+// // Napraviti objekat sa sledecim svojstvima:
+// 1. firstName,
+// 2. lastName,
+// 3. language,
+// 4. setLanguage - metoda za setovanje jezika,
+// 5. setNickName - metoda (svaka osoba ima nadimak sastavljen od prva dva slova
+// imena i prva dva slova prezimena)
+
+const person = {
+  firstName: "Mitar",
+  lastName: "Vranic",
+  language: "Engleski jezik",
+  setLanguage: function (jezik) {
+    this.language = jezik;
+    return this.language;
+  },
+  setNickName: function () {
+    let prvi =
+      this.firstName[0].toUpperCase() + this.firstName[1].toLowerCase();
+    let drugi = this.lastName[0].toLowerCase() + this.lastName[1].toLowerCase();
+    return prvi.concat(drugi);
+  },
+};
+
+console.log(person.setLanguage("Srpski jezik"));
+console.log(person.setNickName());
+
+// 2. Zadatak
+const radnik = {
+  firstName: "Mitar",
+  lastName: "Vranic",
+  fullname: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  adresa: {
+    ulica: "Avnoja",
+    broj: "bb",
+    grad: "Novi Pazar",
+    getAdress: function () {
+      return `${radnik.firstName} ${radnik.lastName} zivi u ulici ${this.ulica} ${this.broj} u ${this.grad} `;
+    },
+  },
+};
+
+console.log(radnik.fullname());
+console.log(radnik.adresa.getAdress());
+// 3. Zadatak
+// const automobil = {
+//     marka: "Audi",
+//     model: "Q7",
+//     boja: "Bela",
+//     pogon: "quattro",
+//     menjac: "Automatik",
+//     km:240000,
+//     vlasnik:[062321552,063930630],
+//     garaza:{
+//         parking:"JKP Servis",
+//         vikend:"od 17 free",
+//         satnaKarta: "50",
+//         dnevnaKarta: "200",
+//         mesecnaKarta: "2000",
+//         platiZa: function(od, do) {
+//           const prviO
+// }
+//     }
+// }
