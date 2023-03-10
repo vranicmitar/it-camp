cc64c43de8ff5855a7db6a4dd9e686581c453d9f
vranicmitar <mitarvranic0@gmail.com>
2023-03-10 13:55:18 +0100

    #updates

--- js/51.cas/domaci.js
+++ js/51.cas/domaci.js
@@ -17,3 +17,24 @@ const checkYears = new Promise((resolve, reject) => {
 });
 
 checkYears.then((por) => console.log(por)).catch((por2) => console.log(por2));
+
+
+
+const checkAge = (person) => {
+  return new Promise((resolve, reject) => {
+    if (person.age >= 18) {
+      resolve("Vi ste punoletni");
+    } else {
+      reject(person.age);
+    }
+  });
+};
+const person1 = {
+  firstName: `Mitar`,
+  lastName: `Vranic0`,
+  age: 20,
+};
+
+checkAge(person1)
+  .then((message) => console.log(message))
+  .catch((age) => console.log(`Osoba ima ${age} godina`));
