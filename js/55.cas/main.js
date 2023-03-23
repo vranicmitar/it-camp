a8d101a488d8702ee36f2ebf72a2e8cbea502a61
vranicmitar <mitarvranic0@gmail.com>
2023-03-23 01:56:38 +0100

    #update dom homework

---
+++ js/55.cas/main.js
@@ -0,0 +1,21 @@
+let input = document.querySelector("#input");
+let button = document.querySelector("#batn");
+let lista = document.querySelector("#lista");
+
+button.addEventListener("click", function () {
+  const textList = input.value;
+  const cerateLi = document.createElement(`li`);
+  const deleteButton = document.createElement(`button`);
+
+  cerateLi.innerText = textList;
+  deleteButton.innerText = `Delete`;
+  deleteButton.addEventListener(`click`, deletebtn);
+
+  function deletebtn(e) {
+    cerateLi.remove();
+  }
+
+  cerateLi.append(deleteButton);
+  lista.append(cerateLi);
+  input.value = "";
+});
