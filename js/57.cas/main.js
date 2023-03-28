44c11823e7fd28b69c99302eebf6ab2020922797
vranicmitar <mitarvranic0@gmail.com>
2023-03-28 15:35:14 +0200

    #finish task Guess my number

--- js/57.cas/main.js
+++ js/57.cas/main.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(`input`);
 const button = document.querySelector(`#prvibutton`);
+const againButton = document.querySelector(`#again-button`);
 
 let score = 20;
 let highscore = 0;
@@ -10,13 +11,37 @@ const poruka = function (poruka) {
 };
 
 button.addEventListener(`click`, function () {
-  if (input.value > 20) {
-    poruka(`Brojevi se pogadjaju od 1 do 20`);
-  } else if (input.value === tajniBroj) {
+  const broj = Number(document.querySelector(`.broj`).value);
+  if (broj > 20 || input.value === "") {
+    poruka(`Morate uneti broj od 1 do 20 !!!`);
+  } else if (broj === tajniBroj) {
     poruka(`Pogodili ste broj`);
-  } else if (input.value > tajniBroj) {
-    poruka(`Previse visoko ste nagadjali !`);
-  } else if (input.value < tajniBroj) {
-    poruka(`Previse nisko ste nagadjali !`);
+    document.querySelector(
+      `body`
+    ).style.backgroundImage = `linear-gradient(to left, red, yellow)`;
   }
+
+  if (score > highscore) {
+    highscore = score;
+    document.querySelector(`.highscore-span`).textContent = highscore;
+  } else if (broj !== tajniBroj) {
+    if (score > 1) {
+      poruka(broj > tajniBroj ? `Previse visoko ` : `Previse nisko`);
+      score--;
+      document.querySelector(`.score-span`).textContent = score;
+    } else {
+      poruka(`Izgubili ste`);
+      document.querySelector(`.score-span`).textContent = 0;
+    }
+  }
+});
+
+againButton.addEventListener(`click`, function () {
+  score = 20;
+  tajniBroj = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".score-span").textContent = score;
+  document.querySelector(".broj").value = "";
+  document.querySelector(
+    `body`
+  ).style.backgroundImage = `linear-gradient(to right, red, yellow)`;
 });
