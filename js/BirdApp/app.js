dece5f00edfedada44edea6a83e8b162763b0170
vranicmitar <mitarvranic0@gmail.com>
2023-03-30 18:04:39 +0200

    #update BIRDTV task

--- js/BirdApp/app.js
+++ js/BirdApp/app.js
@@ -23,10 +23,8 @@ const getVideos = async () => {
   data.Video.forEach((video) => {
     const videoItem = document.createElement(`div`);
     videoItem.className = `video-item`;
-
     const slikaVidea = document.createElement(`img`);
     slikaVidea.src = video.snapshots.sd;
-    
 
     const naslov = document.createElement(`h1`);
     naslov.textContent = video.name;
@@ -34,6 +32,7 @@ const getVideos = async () => {
     const trajanje = document.createElement(`span`);
 
     const link = document.createElement(`a`);
+    link.className = `linkovi`;
     link.href = video.source.hd;
 
     link.appendChild(slikaVidea);
@@ -56,41 +55,3 @@ const getVideos = async () => {
 };
 
 getVideos();
-
-// const cardContainer = document.querySelector(".container");
-// async function getData() {
-//   const response = await fetch(
-//     "https://services.brid.tv/services/get/latest/26456/0/1/25/0.json"
-//   );
-//   const data = await response.json();
-//   console.log(data);
-//   console.log(data.Video[0]);
-//   data.Video.forEach((item, index) => {
-//     const card = document.createElement("div");
-//     card.className = "item1";
-//     const title = document.createElement("h2");
-//     title.textContent = item.name;
-//     const link = document.createElement("a");
-//     link.href = item.source.hd;
-//     const image = document.createElement("img");
-//     image.src = item.snapshots.sd;
-//     link.appendChild(image);
-//     link.appendChild(title);
-//     card.appendChild(link);
-//     cardContainer.appendChild(card);
-//     function pretvori(time) {
-//       let h = Math.trunc(time / 3600);
-//       let m = Math.trunc((time % 3600) / 60);
-//       let s = time - (h * 3600 + m * 60);
-//       if (m > 9 && s > 9) return `0${h}:${m}:${s}`;
-//       else if (m > 9 && s <= 9) return `0${h}:${m}:0${s}`;
-//       else if (m <= 9 && s > 9) return `0${h}:0${m}:${s}`;
-//       else return `0${h}:0${m}:0${s}`;
-//     }
-//     let duration = document.createElement("p");
-//     duration.textContent = pretvori(Number(item.duration));
-//     card.appendChild(duration);
-//   });
-// }
-
-// getData();
